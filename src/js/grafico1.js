77d3f333bc0dce5b06a0d4b1b1895a80b0631a16
gustavo-garcia-dev <gustavo.gribisilva@gmail.com>
2026-06-18 23:44:26 -0300

    atualizando grafico

--- src/js/grafico1.js
+++ src/js/grafico1.js
@@ -11,7 +11,48 @@ new Chart(document.getElementById('graficoMensal'), {
 
         datasets: [{
             label: 'Gastos',
-            data: [1200, 1800, 900, 2100]
+            data: [1200, 1800, 900, 2100],
+            backgroundColor: '#00C853',
+            borderRadius: 10
         }]
+     },
+
+    options: {
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'rgba(255, 255, 255, 0.5)',
+                    font: {
+                        size: 20
+                    }
+                }
+            }
+        },
+
+        scales: {
+            x: {
+                ticks: {
+                    color: 'rgba(255, 255, 255, 0.5)',
+                    font: {
+                        size: 20
+                    }
+                },
+                grid: {
+                    color: 'rgba(255, 255, 255, 0.5)'
+                }
+            },
+
+            y: {
+                ticks: {
+                    color: 'rgba(255, 255, 255, 0.5)',
+                    font: {
+                        size: 20
+                    }
+                },
+                grid: {
+                    color: 'rgba(255, 255, 255, 0.5)'
+                }
+            }
+        }
     }
 });
